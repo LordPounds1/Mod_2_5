19fbec6c17c9a90daaf8954dd4af6c2631bab1a1
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:22:37 +0600

    Add project files.

---
+++ Mod_2_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mod_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число:");
+            int n = int.Parse(Console.ReadLine());
+            char[] rev = n.ToString().ToCharArray();
+            Array.Reverse(rev);
+            int res = int.Parse(new string(rev));
+            Console.WriteLine("Число, полученное при инверсии: " + res);
+        }
+    }
+}
